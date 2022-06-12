89b2b3bbc82ce1bcb7f113b01a73e6ee75521a33
aashiquemasum <shopnogrohon@yahoo.com>
2022-06-12 00:36:45 -0400

    my first commit

---
+++ src/Manager.java
@@ -0,0 +1,17 @@
+public class Manager extends Member {
+    private String department;
+
+
+    public Manager(String name, int age, String phoneNumber,
+                   String address, double salary, String department) {
+        super(name, age, phoneNumber, address, salary);
+        this.department = department;
+    }
+        public static void main (String[]args){
+            Employee employee = new Employee("Tom", 25, "555-555-55", "Home", 25631.5, "IT");
+            Manager manager = new Manager("Ron", 30, "555-617-55", "Earth", 69586.5, "IT");
+           employee.printSalary();
+            manager.printSalary();
+        }
+    }
+
---
+++ src/Member.java
@@ -0,0 +1,20 @@
+public class Member {
+    private String name;
+    private int age;
+    private String phoneNumber;
+    private String address;
+    private double salary;
+
+    public Member(String n, int ag, String ph, String ad, double sa) {
+        this.name = n;
+        this.age = ag;
+        this.phoneNumber = ph;
+        this.address = ad;
+        this.salary = sa;
+    }
+
+    public void printSalary() {
+        System.out.println("Salary:" +salary);
+    }
+}
+
